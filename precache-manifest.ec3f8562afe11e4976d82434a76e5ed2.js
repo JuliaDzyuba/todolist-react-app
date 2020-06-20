074be2bd64c5ac8d3c652dc913e24bc2349692ee
JuliaDzyuba <newcom1000.2017@gmail.com>
2020-06-20 13:22:18 +0300

    Updates

---
+++ precache-manifest.ec3f8562afe11e4976d82434a76e5ed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c576d6db52989bc8abd94ca36120edd6",
+    "url": "/todolist-react-app/index.html"
+  },
+  {
+    "revision": "ee67b40661f17fc27deb",
+    "url": "/todolist-react-app/static/css/main.3d9e1393.chunk.css"
+  },
+  {
+    "revision": "d962237e4029515655b5",
+    "url": "/todolist-react-app/static/js/2.40819dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react-app/static/js/2.40819dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee67b40661f17fc27deb",
+    "url": "/todolist-react-app/static/js/main.f7f37e67.chunk.js"
+  },
+  {
+    "revision": "16b03e8e11115d0d9a35",
+    "url": "/todolist-react-app/static/js/runtime-main.8774e3ae.js"
+  }
+]);
